3134ebb11132485ad0b5d482e4d0d35ec1b20eb3
Fernando <info_fotografia@icloud.com>
2024-08-30 14:29:59 +0200

    Primeros pasos con las propinas, añadiendo la cantidad seleccionada de propina

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import MenuItem from './components/MenuItem'
 import OrderContent from './components/OrderContent'
 import OrderTotals from './components/OrderTotals'
+import TipPercentageForm from './components/TipPercentageForm'
 import { menuItems } from './data/db'
 import useOrder from './hooks/useOrder'
 
 function App() {
 
-  const { order, addItem, removeItem } = useOrder()
+  const { order, addItem, removeItem, tip, setTip } = useOrder()
 
 
   return (
@@ -34,6 +35,10 @@ function App() {
             <OrderContent 
               order={order}
               removeItem={removeItem}
+            />
+
+            <TipPercentageForm 
+              setTip={setTip}
             />
 
             <OrderTotals 
